b836ec43e67407dd49fc5a0a06cf94be5fdeca8d
ApprenticeA <117183807+ApprenticeA@users.noreply.github.com>
2023-04-13 08:02:35 +0800

    Add files via upload

---
+++ homework4/project1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+public class LinkedList<T>
+{
+    private Node<T> head;
+
+    public void AddFirst(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        newNode.Next = head;
+        head = newNode;
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node<T> current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.AddFirst(1);
+        list.AddFirst(2);
+        list.AddFirst(3);
+        list.AddFirst(4);
+
+        Console.WriteLine("Printing elements using ForEach:");
+        list.ForEach(item => Console.WriteLine(item));
+
+        int sum = 0;
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+        list.ForEach(item =>
+        {
+            sum += item;
+            min = Math.Min(min, item);
+            max = Math.Max(max, item);
+        });
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Min: {min}");
+        Console.WriteLine($"Max: {max}");
+    }
+}
---
+++ homework4/project2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new Clock();
+
+            // 订阅嘀嗒事件
+            clock.Tick += (sender, e) => Console.WriteLine("Tick...");
+
+            // 订阅响铃事件
+            clock.Alarm += (sender, e) => Console.WriteLine("Alarm! Time to wake up!");
+
+            // 设置闹钟时间为 8:00
+            clock.SetAlarm(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0, 0));
+
+            // 开始闹钟
+            clock.Start();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Clock
+    {
+        // 定义闹钟事件
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+
+        private DateTime _alarmTime;
+
+        // 设置闹钟时间
+        public void SetAlarm(DateTime alarmTime)
+        {
+            _alarmTime = alarmTime;
+        }
+
+        // 开始闹钟
+        public void Start()
+        {
+            while (true)
+            {
+                // 获取当前时间
+                var now = DateTime.Now;
+
+                // 触发嘀嗒事件
+                Tick?.Invoke(this, EventArgs.Empty);
+
+                // 如果当前时间等于闹钟时间，触发响铃事件
+                if (now.Hour == _alarmTime.Hour && now.Minute == _alarmTime.Minute && now.Second == _alarmTime.Second)
+                {
+                    Alarm?.Invoke(this, EventArgs.Empty);
+                    break;
+                }
+
+                // 休眠 1 秒钟
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
